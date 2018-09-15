1bfe57fc6905c2511d936a0e651757d0d8706681
jeastick <43275571+jeastick@users.noreply.github.com>
2018-09-15 13:54:16 -0400

    FINAL

--- Question4.py
+++ Question4.py
@@ -1,28 +1,34 @@
 # ENGR 5696
 # JEFF EASTICK
-print ("Assignment 1 Question 4")
+# 2018/9/11
+# ASSIGNMENT 1 - QUESTION 4
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 N = 50
-n = 40
+n = 5
+
 
 plt.ion()
 canvas = np.zeros((N,N))
 
-# Draw an initial pattern with 1's
+# Hard-code an initial pattern made of 1's
 canvas[20,20] = 1
 canvas[20,21] = 1
 canvas[20,22] = 1
 canvas[20,23] = 1
 canvas[20,24] = 1
 
+canvas[21,22] = 1
+canvas[22,22] = 1
+canvas[23,22] = 1
+canvas[24,22] = 1
+canvas[25,22] = 1
 
 
 def iteration(matrix):
     matrix2 = np.zeros((N,N))
-    # cycle through matrix and check whether flips or not. create new matrix "canvas2" 
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             try:
@@ -31,10 +37,10 @@ def iteration(matrix):
                 right = matrix[i,j+1]
                 down = matrix[i+1,j]
             except IndexError as e:
-                print("caught at border" + str(i) + str(j))
+                print("edge of plot at x = " + str(i) + ", y = " + str(j))
             total = left + up + right + down
             alive = total%2 # if even, alive = 0, make dead. if odd, alive = 1, make alive 
-            matrix2[i,j] = 1 - alive
+            matrix2[i,j] = alive
     return matrix2
 
 
@@ -45,9 +51,10 @@ plt.show()
 
 for i in range(n):
     plt.matshow(canvas,fignum = False)
-    plt.pause(0.01)
+    plt.pause(0.25)
     canvas = iteration(canvas)
 
-
+plt.ioff()
+plt.show()
 
 
