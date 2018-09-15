e86d0d1da55f7cd59d0a272cdd68e309a7446b1e
jeastick <43275571+jeastick@users.noreply.github.com>
2018-09-15 13:32:52 -0400

    idk what i changed

--- Question4.py
+++ Question4.py
@@ -4,21 +4,21 @@ print ("Assignment 1 Question 4")
 
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 N = 50
-n = 10
+n = 40
 
 plt.ion()
 canvas = np.zeros((N,N))
 
-# Draw in an initial pattern with 1's
-canvas[21,24] = 1
-canvas[21,25] = 1
-canvas[21,26] = 1
+# Draw an initial pattern with 1's
+canvas[20,20] = 1
+canvas[20,21] = 1
+canvas[20,22] = 1
+canvas[20,23] = 1
+canvas[20,24] = 1
 
 
-# Display matrix
 
 def iteration(matrix):
     matrix2 = np.zeros((N,N))
@@ -32,21 +32,21 @@ def iteration(matrix):
                 down = matrix[i+1,j]
             except IndexError as e:
                 print("caught at border" + str(i) + str(j))
-
             total = left + up + right + down
             alive = total%2 # if even, alive = 0, make dead. if odd, alive = 1, make alive 
             matrix2[i,j] = 1 - alive
     return matrix2
 
 
+
+# Main code
 plt.matshow(canvas)
 plt.show()
 
 for i in range(n):
     plt.matshow(canvas,fignum = False)
-    plt.pause(0.001)
+    plt.pause(0.01)
     canvas = iteration(canvas)
-    plt.draw()
 
 
 
