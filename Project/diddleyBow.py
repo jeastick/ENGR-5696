5c856ab34083c8217b89ce983bb9695f241b8030
jeastick <43275571+jeastick@users.noreply.github.com>
2018-10-10 22:31:26 -0400

    big update

--- Project/diddleyBow.py
+++ Project/diddleyBow.py
@@ -5,33 +5,90 @@ import matplotlib.pyplot as plt
 import scipy.integrate as integrate
 
 
-T = 1000
-mu = 8700
-yp = 1
-xp = 8
-L = 10
+#### GLOBAL VARIABLES ####
+T = 444           # Tension of material (Newtons)
+mu = 8050          # Density of material (kg/m3)
+yp = 0.005          # Height of "pluck" from neutral axis (m)
+xp = 4           # Location of "pluck" along string (m)
+v = (T*mu)**(0.5)
+L = 10              # Period (string length) (m)
+l = L/2            # Half Period (m)
 
-x = np.linspace(0,L,num=1000)
-print(x)
-
-fx = np.piecewise(x, [ x <= xp, x > xp], [lambda x: yp/xp*x, lambda x: (-yp/(L-xp))*x+yp+yp/(L-xp)*xp])
-print(fx)
-
-
-terms = 10
-bn_coefs = np.zeros(terms) 
-l = L/2
-
-#fucked still:
-# for n in range(terms):
-#     b = 1/l*integrate.quad(lambda n: fx*m.sin(n*m.pi*x/l), 0, 2*l)
-#     print(b)
+x_res = 500 # resolution of x
 
 
+def y0(x):
+    if(x<=xp):
+        return yp/xp*x
+    if(x>xp):  
+        return (-yp/(L-xp))*x+yp+yp/(L-xp)*xp
 
+def sin_term(n,x):
+    return m.sin(n*m.pi*x/l)
+
+# We will calculate Fourier sin series coefficients b_n by 
+
+
+x_range = np.linspace(0,L,num=x_res)
+
+y_init = np.zeros(len(x_range))
+
+for i in range(len(x_range)):
+    y_init[i] = y0(x_range[i])
+
+
+integrand = np.zeros(len(x_range))
+
+coefs = 5
+bn = np.zeros(coefs) #i'll calculate this many bn terms
 
 fig, ax = plt.subplots()
-ax.plot(x,fx)
+ax.plot(x_range,y_init)
+
+for n in range(coefs):
+    print("Looping with n = " + str(n))
+    for i in range(len(x_range)): #generate the function i will take the integral of to find bn
+        integrand[i] = y0(x_range[i])*m.sin(n*m.pi*x_range[i]/L)
+    bn[n] = 1/l*np.trapz(integrand,x_range)
+    print("b" + str(n) + " is " + str(bn[n]))
+    # ax.plot(x_range,integrand)
+
+
+
+y00 = np.zeros(len(x_range))
+
+for i in range(len(x_range)):
+    for n in range(len(bn)):
+        y00[i] = y00[i] + bn[n]*m.sin((n)*m.pi*x_range[i]/L)
+# ax.plot(x_range,y00)
+
+
+
+t_range = np.linspace(0,1,num=10)
+
+
+
+for t_step in range(len(t_range)):
+    yxt = np.zeros(len(x_range))
+    for i in range(len(x_range)):
+        for n in range(len(bn)):
+            yxt[i] = yxt[i] + bn[n]*m.sin((n)*m.pi*x_range[i]/L)*m.cos((n)*m.pi*v*t_step/L)
+    ax.plot(x_range,yxt)
+    
+
+
+
+
+# print("bn array is " + str(bn))
+
+print(y_init)
 plt.show()
+
+
+
+
+
+
+
 
 
