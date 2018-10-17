8be1b0dec81178345a8b463d6e0123b6227d5863
jeastick <43275571+jeastick@users.noreply.github.com>
2018-10-16 20:18:38 -0400

    accessor methods

--- Project/Bow.py
+++ Project/Bow.py
@@ -30,6 +30,8 @@ class bow:
 
         self.t_steps = int(self.sampletime*self.f_res)         # number of time samples
 
+        self.generateBow()
+
     def y0(self,x):
                 if(x<=self.xp):
                     return self.yp/self.xp*x
@@ -52,20 +54,16 @@ class bow:
             self.y_init[i] = self.y0(self.x_range[i])
 
 
-        integrand = np.zeros(len(self.x_range))
+        self.integrand = np.zeros(len(self.x_range))
 
-        self.bn = np.zeros(self.coefs) #i'll calculate this many self.bn terms
-
-        fig, ax = plt.subplots()
-        ax.plot(self.x_range,self.y_init)
+        self.bn = np.zeros(self.coefs) 
 
         for n in range(self.coefs):
             print("Looping with n = " + str(n))
             for i in range(len(self.x_range)): #generate the function i will take the integral of to find self.bn
-                integrand[i] = self.y0(self.x_range[i])*m.sin(n*m.pi*self.x_range[i]/self.L)
-            self.bn[n] = 1/self.l*np.trapz(integrand,self.x_range)
+                self.integrand[i] = self.y0(self.x_range[i])*m.sin(n*m.pi*self.x_range[i]/self.L)
+            self.bn[n] = 1/self.l*np.trapz(self.integrand,self.x_range)
             print("b" + str(n) + " is " + str(self.bn[n]))
-            # ax.plot(self.x_range,integrand)
 
 
 
@@ -74,7 +72,6 @@ class bow:
         for i in range(len(self.x_range)):
             for n in range(len(self.bn)):
                 self.y00[i] = self.y00[i] + self.bn[n]*m.sin((n)*m.pi*self.x_range[i]/self.L)
-        ax.plot(self.x_range,self.y00)
 
 
 
@@ -91,24 +88,62 @@ class bow:
                 # print("self.x_range i is" + str(self.x_range[i]))
                 if(self.x_range[i] == self.pup):
                     self.soundwave[t_step] = yxt[i]*100
-            if(t_step < 300 and t_step%2==0):
-                ax.plot(self.x_range,yxt)
+            # if(t_step < 300 and t_step%2==0):
+                # ax.plot(self.x_range,yxt)
         
-        plt.show()
-
-        # print("Datatype for soundfile number is " + str(type(self.soundwave[2])))
+        # plt.show()
 
         scipy.io.wavfile.write("wave1.wav", self.f_res, self.soundwave)
 
-        print("self.soundwave is " + str(self.soundwave))
-        fig2, ax = plt.subplots()
+## x_range and y_init
+    def plot_y_init(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.x_range,self.y_init)
+        plt.show()
+
+## x_range and self.integrand
+    def plot_integrand(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.x_range,self.integrand)
+        plt.show()
+
+## x_range and y00
+    def plot_y00(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.x_range,self.y00)
+        plt.show()
+
+# ## x_range and yxt over i time steps, skipping j time steps
+#     def plot_yxt(numSteps,skipSteps):
+#         fig, ax = plt.subplots()
+
+#         for t_step in range(len(self.t_range)):
+#             if(t_step < numSteps and t_step%skipSteps = 0):
+#             ax.plot(self.x_range,yxt)
+#         ax.plot(self.x_range,self.y00)
+#         plt.show()
+## t_range and soundwave
+
+    def plot_soundwave(self):
+        fig, ax = plt.subplots()
         ax.plot(self.t_range,self.soundwave)
         plt.show()
 
 
+
+
+
+## animate?
+
+
+#MAIN
             # Tension, LinDens, PluckHeight, PluckLocation, StringLength, PickupLocation, SampleTime, Coefficients):
+
 bow1 = bow(80, 0.020, 0.01, 0.20, 1, 0.5, 0.1, 10)
-bow1.generateBow()
+bow1.plot_y_init()
+bow1.plot_integrand()
+bow1.plot_y00()
+bow1.plot_soundwave()
 
 
 
